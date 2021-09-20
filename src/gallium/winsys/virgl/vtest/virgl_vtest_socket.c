e53688c73374d266357ed49c482c958cefb3723d
CosineMath <boat_jvm@qq.com>
2021-09-20 15:29:52 +0800

    virgl: add new environment variable VIRGL_VTEST_SOCKET_NAME to set vtest socket name.

--- src/gallium/winsys/virgl/vtest/virgl_vtest_socket.c
+++ src/gallium/winsys/virgl/vtest/virgl_vtest_socket.c
@@ -197,6 +197,7 @@ int virgl_vtest_connect(struct virgl_vtest_winsys *vws)
 {
    struct sockaddr_un un;
    int sock, ret;
+   const char* socket_name;
 
    sock = socket(PF_UNIX, SOCK_STREAM, 0);
    if (sock < 0)
@@ -204,7 +205,11 @@ int virgl_vtest_connect(struct virgl_vtest_winsys *vws)
 
    memset(&un, 0, sizeof(un));
    un.sun_family = AF_UNIX;
-   snprintf(un.sun_path, sizeof(un.sun_path), "%s", VTEST_DEFAULT_SOCKET_NAME);
+   socket_name = getenv("VIRGL_VTEST_SOCKET_NAME");
+   if (socket_name == NULL) {
+       socket_name = VTEST_DEFAULT_SOCKET_NAME;
+   }
+   snprintf(un.sun_path, sizeof(un.sun_path), "%s", socket_name);
 
    do {
       ret = 0;
